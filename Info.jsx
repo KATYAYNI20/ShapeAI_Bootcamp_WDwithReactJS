a3dd7bf889dd4eb75aea3eedc66e79f7379e54dd
KATYAYNI20 <81363331+KATYAYNI20@users.noreply.github.com>
2021-06-26 17:12:18 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.jS</h1>
+      <p>A Basic Web development React jS Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
